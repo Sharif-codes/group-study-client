da6708c8aa54047cb7fb071453e57bb1deb342a1
sharif-codes <sharifxenjia@gmail.com>
2023-11-06 15:29:48 +0600

    delete assignment implemented

--- src/pages/SingleAssignment/SingleAssignment.jsx
+++ src/pages/SingleAssignment/SingleAssignment.jsx
@@ -1,18 +1,40 @@
 import axios from "axios";
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import toast from "react-hot-toast";
+
 
 
 const SingleAssignment = () => {
+   const {user}= useContext(AuthContext)
     const singleAssignment = useLoaderData()
-    const {_id,title,description,mark,difficulty,due,thumbnail,createdBy}= singleAssignment
+    const [assignment, setAssignment]= useState(singleAssignment)
+    const {_id,title,description,mark,difficulty,due,thumbnail,createdBy}= assignment
+    const email= user?.email
+    const data= {email,_id}
+    const selected= {data}
     const handleDelete= ()=>{
-        axios.delete(`http://localhost:5000/delete-assignment/${_id}`)
+        axios.delete(`http://localhost:5000/delete-assignment/`,selected)
+        .then(res=>{
+
+            toast.success(res.data.message)
+            const remaining= assignment?.filter(item=> item._id!== _id)
+            setAssignment(remaining)
+
+        } )
+        .catch(err=>
+            {
+                toast.error(err.response.data.message)
+                
+            } )
+      
     }
 
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <img src={thumbnail} className="w-fit rounded-lg shadow-2xl" />
+                <img src={thumbnail} className="w-72 rounded-lg shadow-2xl" />
                 <div className="space-y-0 lg:space-2">
                     <h1 className="text-5xl font-bold">{title}</h1>
                     <p className="py-6">{description}</p>
